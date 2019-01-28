b38945125750b67e6e1efcad544d32e4c1ad9057
Dayanira Paniagua <dashpame@gmail.com>
2019-01-28 15:26:11 -0700

    all done

--- assignments/02-bash-scripting/cat-n.sh
+++ assignments/02-bash-scripting/cat-n.sh
@@ -3,15 +3,19 @@
 set -u
 
 if [[ $# -eq 0 ]]; then
-    echo "Usage: foo.sh ARG"
+    echo "Usage: cat-n.sh does not have an argument"
     exit 1
 fi
 
 FILE=$1
 
 if [[ ! -f "$FILE" ]]; then
-    echo "$FILE is NOT a file"
+    echo "$FILE is not a file"
     exit 1
 fi
 
-
+i=0
+while read -r LINE; do
+	i=$((i+1))
+	echo $i "$LINE"
+done < "$FILE"
--- assignments/02-bash-scripting/head.sh
+++ assignments/02-bash-scripting/head.sh
@@ -3,15 +3,23 @@
 set -u
 
 if [[ $# -eq 0 ]]; then
-    echo "Usage: foo.sh ARG"
+    echo "Usage: head.sh ARG"
     exit 1
 fi
 
 FILE=$1
+NUM_LINES=${2:-3}
 
 if [[ ! -f "$FILE" ]]; then
-    echo "$FILE is NOT a file"
+    echo "$FILE is not a file"
     exit 1
 fi
 
-
+i=0
+while read -r LINE; do
+	echo "$LINE"
+	i=$((i+1))
+	if [[ $i -eq $NUM_LINES ]]; then
+		break
+	fi
+done < "$FILE"
